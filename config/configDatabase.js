b550a74f77e3cdf820dbffc3956d34a7fc640298
KyleBing <kylebing@163.com>
2022-04-27 11:47:19 +0800

    ^

--- config/configDatabase.js
+++ config/configDatabase.js
@@ -1,7 +1,7 @@
 module.exports = {
     host:       'localhost',
     user:       'root',
-    password:   'nnqi',
+    password:   '----',
     port:       '3306',
     database:   'diary',
     invitation: '----',
