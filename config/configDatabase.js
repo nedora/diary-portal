8f4e83ae991015740860a515d3d05840a2267688
邴新科 <kylebing@163.com>
2022-05-11 09:17:15 +0800

    添加用户统计数据查询

--- config/configDatabase.js
+++ config/configDatabase.js
@@ -6,5 +6,6 @@ module.exports = {
     database:   'diary',
     invitation: '----',
     multipleStatements: true, // 允许同时请求多条 sql 语句
-    timezone: ''
+    timezone: '',
+    adminCount: 'kylebing@163.com' // 管理员账户，用于查看所有用户统计数据
 }
