812ec351b96fda1fb75221da0ba7ebe683c56db8
邴新科 <kylebing@163.com>
2022-05-19 16:22:10 +0800

    分离数据库参数和项目参数

--- config/configDatabase.js
+++ config/configDatabase.js
@@ -4,8 +4,6 @@ module.exports = {
     password:   '----',
     port:       '3306',
     database:   'diary',
-    invitation: '----',
     multipleStatements: true, // 允许同时请求多条 sql 语句
-    timezone: '',
-    adminCount: 'kylebing@163.com' // 管理员账户，该用户可以在统计页面中查看所有用户统计数据
+    timezone: ''
 }
---
+++ config/configProject.js
@@ -0,0 +1,8 @@
+module.exports = {
+    invitation: '----',
+    adminCount: 'kylebing@163.com', // 管理员账户，该用户可以在统计页面中查看所有用户统计数据
+
+    // BandwagonHost Info
+    vpsVEID: '----',
+    vpsApiKey: '----',
+}
