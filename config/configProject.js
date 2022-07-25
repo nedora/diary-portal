5effcbe25578348682c13c1aad6cf83dd19925cc
邴新科 <kylebing@163.com>
2022-07-25 10:52:02 +0800

    diary-category init

--- config/configProject.js
+++ config/configProject.js
@@ -1,8 +1,4 @@
 module.exports = {
     invitation: '----',
-    adminCount: 'kylebing@163.com', // 管理员账户，该用户可以在统计页面中查看所有用户统计数据
-
-    // BandwagonHost Info
-    vpsVEID: '----',
-    vpsApiKey: '----',
+    adminCount: 'kylebing@163.com' // 管理员账户，该用户可以在统计页面中查看所有用户统计数据
 }
