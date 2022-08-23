ddcf00445707b939a19e8bedb3c48302f716ee99
邴新科 <kylebing@163.com>
2022-08-23 14:38:58 +0800

    fix: error

--- routes/bill.js
+++ routes/bill.js
@@ -31,6 +31,7 @@ router.get('/', (req, res, next) => {
         })
 })
 
+
 router.get('/sorted', (req, res, next) => {
     utility.verifyAuthorization(req)
         .then(verified => {
@@ -48,14 +49,14 @@ router.get('/sorted', (req, res, next) => {
                         and month(date) = ${month}
                         and category = 'bill'
                         and uid = ${req.query.uid}
-                        order by date desc;
+                        order by date asc;
                     `)
             }
-            sqlRequests.push(utility.getDataFromDB(sqlArray))
+            sqlRequests.push(utility.getDataFromDB( 'diary', sqlArray))
             // 这里有个异步运算的弊端，所有结果返回之后，我需要重新给他们排序，因为他们的返回顺序是不定的。难搞哦
             Promise.all(sqlRequests)
                 .then(values => {
-                    let response = []
+                    let responseData = []
                     let afterValues = values[0].filter(item => item.length > 0) // 去年内容为 0 的年价数据
                     afterValues.forEach(daysArray => {
 
@@ -63,6 +64,11 @@ router.get('/sorted', (req, res, next) => {
                         let monthSum = 0
                         let monthSumIncome = 0
                         let monthSumOutput = 0
+                        let food = {
+                            breakfast: 0, // 早餐
+                            launch: 0, // 午餐
+                            dinner: 0 // 晚饭
+                        }
 
                         // 用一次循环处理完所有需要在循环中处理的事：合总额、map DayArray
                         daysArray.forEach(item => {
@@ -71,20 +77,29 @@ router.get('/sorted', (req, res, next) => {
                             monthSum = monthSum + processedDayData.sum
                             monthSumIncome = monthSumIncome + processedDayData.sumIncome
                             monthSumOutput = monthSumOutput + processedDayData.sumOutput
+                            food.breakfast = food.breakfast + processedDayData.items.filter(item => item.item.indexOf('早餐') > -1).reduce((a,b) => a.price || 0 + b.price || 0, 0)
+                            food.launch = food.launch + processedDayData.items.filter(item => item.item.indexOf('午餐') > -1).reduce((a,b) => a.price || 0 + b.price || 0, 0)
+                            food.dinner = food.dinner + processedDayData.items.filter(item => item.item.indexOf('晚餐') > -1).reduce((a,b) => a.price || 0 + b.price || 0, 0)
                         })
 
-                        response.push({
+                        responseData.push({
                             id: daysArray[0].id,
                             month: daysArray[0].month,
                             count: daysArray.length,
                             days: daysData,
-                            sum: monthSum,
-                            sumIncome: monthSumIncome,
-                            sumOutput: monthSumOutput
+                            sum: money(monthSum),
+                            sumIncome: money(monthSumIncome),
+                            sumOutput: money(monthSumOutput),
+                            food: {
+                                breakfast: money(food.breakfast),
+                                launch: money(food.launch),
+                                dinner: money(food.dinner),
+                                sum: money(food.breakfast + food.launch + food.dinner)
+                            }
                         })
                     })
-                    response.sort((a, b) => a.year > b.year ? 1 : -1)
-                    res.send(new ResponseSuccess(response))
+                    responseData.sort((a, b) => a.year > b.year ? 1 : -1)
+                    res.send(new ResponseSuccess(responseData))
                 })
                 .catch(err => {
                     res.send(new ResponseError(err, err.message))
@@ -110,7 +125,7 @@ function processBillOfDay(billContent, date){
     }
     strArray.forEach(item => {
         let itemInfos = item.split(' ')
-        let price = Number(itemInfos[1]) || 0
+        let price = Number(itemInfos[1]) || 0 // 避免账单填写出错的情况
         if (price < 0) {
             response.sumOutput = response.sumOutput + price
         } else {
@@ -125,6 +140,10 @@ function processBillOfDay(billContent, date){
     })
 
     return response
+}
+
+function money(number){
+    return Number(number.toFixed(2))
 }
 
 module.exports = router
