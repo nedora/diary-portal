359904d96e71899f4a3c657ceccbe5333cf76a9d
邴新科 <kylebing@163.com>
2022-12-30 17:43:25 +0800

    projectConfig 添加 七牛云配置

--- config/configProject.js
+++ config/configProject.js
@@ -2,4 +2,8 @@ module.exports = {
     invitation: '----',
     adminCount: 'kylebing@163.com', // 管理员账户，该用户可以在统计页面中查看所有用户统计数据
     TOKEN_NAME: 'Diary-Token', // header 中的 token 字段
+
+    // 七牛云密钥
+    qiniuAccessKey: '---',
+    qiniuSecretKey: '---',
 }
